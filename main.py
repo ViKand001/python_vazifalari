9c829f96dfc9a329ab925167815630c8b32977b1
ViKand001 <joldasovasilbek28@gmail.com>
2023-05-04 21:03:28 +0500

    davomi

--- main.py
+++ main.py
@@ -2,6 +2,106 @@
 """
 Created on Tue May  2 15:58:29 2023
 
-@author: Kamalov M
+@ViKAnd01
 """
+
+
+#import avto_info_mod
+
+#avto1 = avto_info_mod.avto_info("GM", "Malibu", "Qora", "Avtomat", 2020 , 40000)
+#avto_info_mod.info_print(avto1)
+ 
+
+
+#import avto_info_mod as aim
+#avto1 =aim.avto_info("GM", "Malibu", "Qora", "Avtomat", 2020 , 4000)
+#aim.info_print(avto1)
+
+#from avto_info_mod import avto_info, info_print
+#avto1 = avto_info("GM", "Malibu", "Qora", "Avtomat", 2020 , 4000)
+#info_print(avto1)
+
+#from avto_info_mod import avto_info as ainfo, info_print as iprint
+#avto1 = ainfo("GM", "Malibu", "Qora", "Avtomat", 2020 , 4000)
+#iprint(avto1)
+
+#from avto_info_mod import *
+
+#avto1 = avto_info("GM", "Malibu", "Qora", "Avtomat", 2020 , 4000)
+#info_print(avto1)
+
+
+#import math
+
+#x=400
+#print(math.sqrt(x))
+#print(math.pow(5,15))
+#print(math.pi)
+#print(math.log2(8))
+#print(math.log10(100))
+
+#import random as r
+#randint()
+#son = r.randint(0,100)
+#print(son)
+
+#choice()
+#ismlar = ['Olim', 'Anvar', 'Hasan', 'Husan']
+#ism = r.choice(ismlar)
+#print(ism)
+
+#print(r.choice(ism))
+
+
+#x = list(range(0,51,5))
+#print(x)
+#print(r.choice(x))
+
+#shuffle()
+#x =list(range(11))
+#rint(x)
+#r.shuffle(x)
+#print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
